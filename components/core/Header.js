cb6f7fee98730a712d95bc000718c0b37926ecb5
kritical0613 <kritical0613@outlook.com>
2022-03-16 14:29:13 -0500

    fix: prepare deployment

--- components/core/Header.js
+++ components/core/Header.js
@@ -6,13 +6,75 @@ import ButtonBase from '@mui/material/ButtonBase';
 import KeyboardArrowUp from '@mui/icons-material/KeyboardArrowUp'
 import MuiNextLink from './MuiNextLink'
 import Image from 'next/image'
-import Navbar from '/Navbar'
+//import Navbar from '/Navbar'
 import SideDrawer from './SideDrawer'
 // import HideOnScroll from './HideOnScroll'
 import Fab from '@mui/material/Fab'
 import { useState, useEffect } from 'react'
 import BackToTop from './BackToTop'
 import Connect from '../web3/connect'
+
+//import Stack from "@mui/material/Stack";
+//import Toolbar from "@mui/material/Toolbar";
+import { useRouter } from "next/router";
+
+const Navbar = ({ navLinks }) => {
+
+  const router = useRouter()
+
+  const handleGoSection = (index) => {
+    if (router.pathname === '/') {
+      const element = document.getElementById(index)
+      const topPos = element.offsetTop
+      window.scrollTo({
+        top: topPos - 90,
+        left: 100,
+        behavior: 'smooth'
+      })
+    } else {
+      router.push(`/#${index}`)
+      // handleGoSection(index)
+    }
+  }
+
+  return (
+    <Toolbar
+      component="nav"
+      sx={{
+        display: { xs: `none`, md: `flex` },
+      }}
+    >
+      <Stack direction="row" sx={{ gap: "49px" }}>
+        {navLinks.map(({ title, path }, i) => (
+          <Stack
+            key={`${title}${i}`}
+            onClick={() => handleGoSection(path)}
+            variant="button"
+            underline="none"
+            sx={[
+              {
+                cursor: 'pointer',
+                fontFamily: 'Chubby Choo',
+                fontSize: '24px',
+                opacity: 0.7,
+                color: 'primary.light',
+                textTransform: 'none',
+                transition: 'all 300ms ease-in'
+              },
+              {
+                '&:hover': {
+                  opacity: 1
+                }
+              }
+            ]}
+          >
+            {title}
+          </Stack>
+        ))}
+      </Stack>
+    </Toolbar>
+  );
+};
 
 const Offset = styled('div')(({ theme }) => theme.mixins.toolbar)
 
